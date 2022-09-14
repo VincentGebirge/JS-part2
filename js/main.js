e5131142038f613bc169d56834fefb2e97a9d494
VincentGebirge <vincent.kapin@gmail.com>
2022-09-15 00:22:43 +1000

    Lesson 1

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const products = [
+    {id: 1, title: 'Notebook', price: 2000, image: 'https://avatars.mds.yandex.net/get-mpic/4080064/img_id4171773565509789839.jpeg/orig'},
+    {id: 2, title: 'Mouse', price: 20, image: 'https://cdn.svyaznoy.ru/upload/iblock/759/08-29-2013_12-52_apple-magic-mouse%20zoom.jpg/resize/483x483/hq/'},
+    {id: 3, title: 'Keyboard', price: 200, image: 'https://c.dns-shop.ru/thumb/st1/fit/500/500/e94fa004a6095cd9c50dd9fdc681b1b2/a9642abc96b50cbae6a51df39174525808bfa963d54a6028aef314ca801f107c.jpg.webp'},
+    {id: 4, title: 'Gamepad', price: 50, image: 'https://avatars.mds.yandex.net/get-mpic/4055688/img_id8137017270087126256.jpeg/orig'},
+];
+//Функция для формирования верстки каждого товара
+//Добавить в выводе изображение
+const renderProduct = (el) => {
+    return `<div class="product-item">
+                <div class="img">
+                  <img src="${el.image}" alt="" class="product-img">
+                </div>
+                <h3>${el.title}</h3>
+                <p>${el.price}</p>
+                <button class="buy-btn">Купить</button>
+            </div>`
+};
+const renderPage = list => {
+    const productsList = list.map(item => renderProduct(item));
+    console.log(productsList);
+    document.querySelector('.products').innerHTML = productsList.join('');
+};
+
+renderPage(products);
